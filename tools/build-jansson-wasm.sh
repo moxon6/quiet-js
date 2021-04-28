b948f67364a3e9f896af28bccec8ca0cc222dd0d
Martin Moxon <moxon6@googlemail.com>
2021-04-28 17:32:01 +0100

    Remove scripts from tools

--- tools/build-jansson-wasm.sh
+++
@@ -1,7 +0,0 @@
-source ~/dev/emsdk/emsdk_env.sh
-
-cd jansson-2.13
-emconfigure ./configure --prefix=${HOME}/opt
-
-make
-make install
--- tools/build-libfec-wasm.sh
+++
@@ -1,7 +0,0 @@
-source ~/dev/emsdk/emsdk_env.sh
-
-cd libfec
-emconfigure ./configure --prefix=${HOME}/opt
-sed -i 's/ldconfig//g' makefile
-make
-make install

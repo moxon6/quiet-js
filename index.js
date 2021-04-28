122fb38f1d049059e5c06c1f38d28b47e51be806
Martin Moxon <moxon6@googlemail.com>
2021-04-28 01:49:20 +0100

    Conditionally use modulePath

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import Quiet from './quiet';
 export * from './utils';
 
 export default async function(profiles, audioContext, modulePath) {
-    const wasmModule = await Module({
-        locateFile: () => modulePath,
-    });
+    const wasmModule = await (
+        modulePath 
+            ? Module({ locateFile: () => modulePath })
+            : Module()
+    );
     return new Quiet(profiles, audioContext, wasmModule);
 }

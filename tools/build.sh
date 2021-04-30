93e54abd20ec361ab05545184a534f61f7eb6f16
Martin Moxon <moxon6@googlemail.com>
2021-04-30 16:35:35 +0100

    Add build.sh

---
+++ tools/build.sh
@@ -0,0 +1,14 @@
+em++ \
+    -O2 \
+    -I${HOME}/opt/include \
+    -L${HOME}/opt/lib \
+    -lquiet \
+    -lliquid \
+    -ljansson \
+    -lfec \
+    -o build.js \
+    --no-entry \
+    -s MODULARIZE=1 \
+    -s STRICT=1 \
+    -s EXPORTED_FUNCTIONS='["_free","_malloc","_quiet_encoder_clamp_frame_len","_quiet_encoder_create","_quiet_encoder_destroy","_quiet_encoder_emit","_quiet_encoder_get_frame_len","_quiet_encoder_profile_str","_quiet_encoder_send"]' \
+    -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap", "intArrayFromString"]'

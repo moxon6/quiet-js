861ab0c544031da8b44387e293679a18afbb72f0
Martin Moxon <moxon6@googlemail.com>
2021-05-02 01:53:15 +0100

    Avoid using ccall for parts of interop

--- interop.js
+++ interop.js
@@ -1,13 +1,13 @@
 export default (imodule) => ({
     free: (opt) => imodule.ccall('free', null, ['pointer'], [opt]),
     malloc: (bufferSize) => imodule.ccall('malloc', 'pointer', ['number'], [bufferSize]),
-    quietEncoderClampFrameLen: (encoder, sampleBufferSize) => imodule.ccall('quiet_encoder_clamp_frame_len', 'number', ['pointer', 'number'], [encoder, sampleBufferSize]),
-    quietEncoderCreate: (opt, sampleRate) => imodule.ccall('quiet_encoder_create', 'pointer', ['pointer', 'number'], [opt, sampleRate]),
-    quietEncoderDestroy: (encoder) => imodule.ccall('quiet_encoder_destroy', null, ['pointer'], [encoder]),
-    quietEncoderEmit: (encoder, samples, sampleBufferSize) => imodule.ccall('quiet_encoder_emit', 'number', ['pointer', 'pointer', 'number'], [encoder, samples, sampleBufferSize]),
-    quietEncoderGetFrameLen: (encoder) => imodule.ccall('quiet_encoder_get_frame_len', 'number', ['pointer'], [encoder]),
+    quietEncoderClampFrameLen: (encoder, sampleBufferSize) => imodule._quiet_encoder_clamp_frame_len(encoder, sampleBufferSize),
+    quietEncoderCreate: (opt, sampleRate) => imodule._quiet_encoder_create(opt, sampleRate),
+    quietEncoderDestroy: (encoder) => imodule._quiet_encoder_destroy(encoder),
+    quietEncoderEmit: (encoder, samples, sampleBufferSize) => imodule._quiet_encoder_emit(encoder, samples, sampleBufferSize),
+    quietEncoderGetFrameLen: (encoder) => imodule._quiet_encoder_get_frame_len(encoder),
     quietEncoderProfileStr: (cProfiles, cProfile) => imodule.ccall('quiet_encoder_profile_str', 'pointer', ['array', 'array'], [cProfiles, cProfile]),
-    quietEncoderSend: (encoder, frame, byteLength) => imodule.ccall('quiet_encoder_send', 'number', ['pointer', 'array', 'number'], [encoder, frame, byteLength]),
+    quietEncoderSend: (encoder, frame, byteLength) => imodule._quiet_encoder_send(encoder, frame, byteLength),
     subArray: (start, finish) => imodule.HEAPF32.subarray(start, finish),
   });
   

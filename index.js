2cb63d55fdf4348ac72b330d9a44cf3d4bf9f194
Martin Moxon <moxon6@googlemail.com>
2021-05-03 16:34:13 +0100

    Build quiet.wasm

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import Quiet from './quiet';
-import getInstance from './build'
+
+const importObj = {
+    env: {
+      __sys_getpid: () => null,
+    },
+    wasi_snapshot_preview1: {
+      proc_exit: () => null,
+      clock_time_get: () => null,
+      fd_close: () => null,
+      fd_write: () => null,
+      fd_seek: () => null,
+      fd_read: () => null,
+    },
+};
 
 export default async function(audioContext, quietWasm) {
-    const instance = await getInstance(quietWasm);
+    const response = await fetch(quietWasm);
+    const { instance } = await WebAssembly.instantiateStreaming(response, importObj);
     return new Quiet(audioContext, instance);
 }

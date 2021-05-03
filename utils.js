1acddd4cc34860276e1b10b31b782a456319c8b3
Martin Moxon <moxon6@googlemail.com>
2021-05-03 16:09:11 +0100

    Remove unused utils.js

--- utils.js
+++ utils.js
@@ -1,16 +1,3 @@
-export const total = (arr) => arr.reduce((a, b) => a + b, 0);
-
-export const mean = (arr) => total(arr) / arr.length;
-
-export function str2ab(s) {
-  const sUtf8 = unescape(encodeURIComponent(s));
-  const buf = new ArrayBuffer(sUtf8.length);
-  const bufView = new Uint8Array(buf);
-  for (let i = 0; i < sUtf8.length; i += 1) {
-    bufView[i] = sUtf8.charCodeAt(i);
-  }
-  return buf;
-}
 
 export function resumeIfSuspended(audioCtx) {
   if (audioCtx.state === 'suspended') {

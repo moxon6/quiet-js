7242908ff8f879275606e9c41158fbcb98b9cc38
Martin Moxon <moxon6@googlemail.com>
2021-05-10 08:55:37 +0100

    Export additional functions

--- tools/build.sh
+++ tools/build.sh
@@ -9,4 +9,16 @@ em++ \
     --no-entry \
     -s MODULARIZE=1 \
     -s STRICT=1 \
-    -s EXPORTED_FUNCTIONS='["_free","_malloc","_quiet_encoder_clamp_frame_len","_quiet_encoder_create","_quiet_encoder_destroy","_quiet_encoder_emit","_quiet_encoder_get_frame_len","_quiet_encoder_profile_str","_quiet_encoder_send"]'
+    -s EXPORTED_FUNCTIONS='[
+        "_free",
+        "_malloc",
+        "_quiet_encoder_clamp_frame_len",
+        "_quiet_encoder_create",
+        "_quiet_encoder_destroy",
+        "_quiet_encoder_emit",
+        "_quiet_encoder_get_frame_len",
+        "_quiet_encoder_profile_str",
+        "_quiet_encoder_send",
+        "_quiet_decoder_profile_str",
+        "_quiet_decoder_create"
+    ]'
